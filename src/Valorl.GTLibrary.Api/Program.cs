1945b184d4a1ed2c905c7d105ee92b781654eaf1
Valer Orlovsky <valer1212@gmail.com>
2017-02-28 20:53:51 +0100

    Initial

--- src/Valorl.GTLibrary.Api/Program.cs
+++ src/Valorl.GTLibrary.Api/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 
-namespace Valorl.GTLibrary
+namespace Valorl.GTLibrary.Api
 {
     public class Program
     {
