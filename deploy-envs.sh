6819b238c6c1000fe8222085069b6236e9f2b18b
Valer Orlovsky <valer1212@gmail.com>
2017-02-28 22:10:55 +0100

    restructure + add AWS deploy scripts

---
+++ deploy-envs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# set environment variables used in deploy.sh and AWS task-definition.json:
+export IMAGE_NAME=valorl-gtlibrary-backend
+export IMAGE_VERSION=latest
+
+export AWS_DEFAULT_REGION=eu-west-1b
+export AWS_ECS_CLUSTER_NAME=default
+export AWS_VIRTUAL_HOST=api.gtl.enartee.online
+
+# set any sensitive information in travis-ci encrypted project settings:
+# required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+# optional: SERVICESTACK_LICENSE
