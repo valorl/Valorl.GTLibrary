f4d92a7b51127c80d339b6b2a84368a65439204b
Valer Orlovsky <valer1212@gmail.com>
2017-03-02 22:56:21 +0100

    back to api.gtl

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=eu-west-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=gtl.enartee.online
+export AWS_VIRTUAL_HOST=api.gtl.enartee.online
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
