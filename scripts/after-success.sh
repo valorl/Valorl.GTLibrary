741cf610f166b8a5321ab31d498113bbc2ee1f49
Valer Orlovsky <valer1212@gmail.com>
2017-04-17 18:44:21 +0200

    after-success script fix

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dotnet publish -c Release ../src/Valorl.GTLibrary.Api/Valorl.GTLibrary.Api.csproj
+# dotnet publish -c Release ../src/Valorl.GTLibrary.Api/Valorl.GTLibrary.Api.csproj
 source ./deploy-envs.sh
 source ./docker-build.sh
-if [ "$TRAVIS_BRANCH" == "master" ]; then ./deploy.sh; fi
+if [ "$TRAVIS_BRANCH" == "master" ]; then source ./deploy.sh; fi
